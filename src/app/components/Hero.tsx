bf1aaebf5c8a61cbd68a4cec3efc9b963c50b933
Gaelle Boucher <gaelleboucher.dev@gmail.com>
2026-04-29 18:14:27 +0200

    ajout nouvelle PP

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -275,7 +275,7 @@ export default function Hero() {
                                 }}
                             >
                                 <Image
-                                    src="/assets/gaelle-new.jpg"
+                                    src="/assets/gaelle-new.png"
                                     alt="Gaëlle Boucher - Développeuse web éditoriale"
                                     className="w-full h-full object-cover object-[47.9%_24.1%]"
                                     width={500}
